6fec6b3fedd0368721944c45ed2a6819dc45bf6c
KiceAmber <kiceamber@gmail.com>
2024-01-06 12:39:02 +0800

    add neofetch config file

--- arch/arch_install.sh
+++ arch/arch_install.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+# 输出文本颜色
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+# 输出帮助
+print_help() {
+	echo "Archiso auto install and configure script by ${yellow} KiceAmber ${plain}"
+}
+
+# ================ 全局变量 ==================
+# 是否为 UEFI 模式 1-是 0-不是，默认为 1
+IS_UEFI='1'
+
+# ================ 安装前准备 ==================
+# 检测是否为 UEFI 模式
+
+# 检测时间并校准
+
+# 关闭 reflector 服务
+
+# 磁盘分区
+
+# 手动设置 mirrorlist 列表
+
+# 安装基本的 linux 包
